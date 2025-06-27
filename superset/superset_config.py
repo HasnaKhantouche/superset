c7380d35e5c02c0dcbff31a7a4d0bffacb2a0840
Hasna Khantouche <166396644+HasnaKhantouche@users.noreply.github.com>
2025-06-27 13:01:50 +0100

    Update superset_config.py

--- superset/superset_config.py
+++ superset/superset_config.py
@@ -1,1 +1,3 @@
 SECRET_KEY = 't7QzqxZDOLRsBxBGUQlwtRw8Fd3F960zPHhBh++8PaM8AUxzzWsDzIap'
+SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://superset_db_rabx_user:cpaLhYZnr3kNcyfCvlBeEOiym3gSzLOW@dpg-d1dt1gre5dus73duo810-a.frankfurt-postgres.render.com:5432/superset_db_rabx"
+
